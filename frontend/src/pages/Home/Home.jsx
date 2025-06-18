c87313f6145762684d7abc0c2bc8c8b3d9a55e4b
BellaNH <bouchranahnah@gmail.com>
2025-06-18 17:44:22 +0100

    fix naming issue

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Navbar from "../../components/navbar/navbar";
+import Navbar from "../../components/Navbar/Navbar";
 import "./Home.css"
 import Header from "../../components/Header/Header";
 import ExploreFood from "../../components/ExploreFood/ExploreFood";
