be068ef85c947ae2c5f2bc570b159e875b9b298d
BellaNH <bouchranahnah@gmail.com>
2025-06-19 12:04:08 +0100

    changes the loading of data

--- frontend/src/components/FoodList/FoodList.jsx
+++ frontend/src/components/FoodList/FoodList.jsx
@@ -10,11 +10,13 @@ const FoodList = ({selectedCategory})=> {
     <div className="foodlist-container">
         <h2 className="foodlist-title">Top dishes near you</h2>
         <div className="foodlist">
-            {list.length > 0 && (
+            {list.length > 0 ? (
             list.map((item,index)=>{
                 if(selectedCategory==="All" || selectedCategory===item.category)
                     return <FoodItem item={item} key={index}/>
             }))
+            :
+            <p>Food list is loading ...</p>
         }
         </div>
     </div>
